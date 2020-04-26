b6de0ed2046577a2524a7987d2649c6231543b0b
wegfawefgawefg <668es218pur@gmail.com>
2020-04-25 17:57:51 -0700

    FINALLY true speedy batch learning

--- stage 5/cartpole_sequential_ExpReplay_dep.py
+++ stage 5/cartpole_sequential_ExpReplay_dep.py
@@ -12,25 +12,15 @@ import architectures as arc
 from collections import deque, namedtuple
 import numpy as np
 import termBar as tbar
-import datetime 
 
-'''
-TODO:
-*   add offline mode
-o   make it support other environments
-try convolutional version
-try one that works on pixels
-add checkpoints to prevent forgetting
-make snake environment
-'''
-
-'''
-IDEAS:
-let the network set epsilon somehow?
-give it a memory of more than one frame
-    right now we are relying on experience replay to be the memory, but its input could also include recent frames
-        this way it can choose how important each of the distances into the past are
-'''
+###############################################################################
+#   !!!!! WARNING !!!!!
+#   THIS VERSION IS SLOW AS ASS
+#   IT EXISTS ONLY AS A DEMONSTRATION OF SEQUENTIALLY LEARNING EACH EXPERIENCE
+#       CONVERGENCE IS FASTER, SO HIGHER SAMPLE EFFICIENCY
+#       BUT IT RUNS LIKE 100-1000 TIMES SLOWER AND THAT SCALES WITH BATCH SIZE
+#       SO ITS NOT WORTH IT
+################################################################################
 
 ############################################################
 ####    HELPERS
@@ -48,12 +38,12 @@ GAMMA = 0.95
 
 # MEMORY_SIZE = 1000000
 MEMORY_SIZE = 7000
+# BATCH_SIZE = 20
 BATCH_SIZE = 64
 
 EPSILON_MAX = 1.0
 EPSILON_MIN = 0.01
 EPSILON_DECAY = 0.995
-# EPSILON_DECAY = 1.0
 # EPSILON_DECAY = 0.998
 
 NUM_TRAINING_EPSISODES = 300
@@ -63,28 +53,22 @@ MemoryFrame = namedtuple('MemoryFrame',
 DONE_TRAINING_SCORE = 499
 CONSECUTIVE_WIN_REQUIREMENTS = 5
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
 ############################################################
 ####    SETUP
 ############################################################
-#    setup env
-env = gym.make('CartPole-v1')
-NUM_ACTIONS = env.action_space.n
-NUM_OBS = env.observation_space.shape[0]
-print("Num Actions: " + str(NUM_ACTIONS))
-print("Num Obs: " + str(NUM_OBS))
-
 #   setup net
 # net = arc.FlexNet(4, 2, (24,))
 # net = arc.FlexNet(4, 2, (64,64, 64))
-net = arc.TwoNet(NUM_OBS, NUM_ACTIONS, 24).to(device)
+net = arc.TwoNet(4, 2, 24)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 memoryBank = deque(maxlen=MEMORY_SIZE)
 epsilon = EPSILON_MAX
 lastNScores = deque(maxlen=CONSECUTIVE_WIN_REQUIREMENTS)
 
+env = gym.make('CartPole-v1')
+print(env.action_space)
+print(env.observation_space)
 
 ############################################################
 ####    TRAIN
@@ -96,15 +80,13 @@ recordReward, recordTimesteps, episode = 0, 0, 0
 finishedTraining = False
 while not finishedTraining:
     obs = env.reset()
-    obsFrame = torch.tensor(obs, dtype=torch.float32).view(-1, NUM_OBS).to(device)
+    obsFrame = torch.tensor(obs, dtype=torch.float32).view(-1, 4)
 
     #   run the game
     #   #   break out and start another on fail
     done = False
     frameNum, totalReward = 0, 0
     while True:
-        env.render()
-        # stepCompute_time_start = datetime.datetime.now()
         frameNum += 1
 
         #   pick next action
@@ -117,7 +99,7 @@ while not finishedTraining:
 
         #   tic game
         nextObs, reward, done, info = env.step(action)
-        nextObsFrame = torch.tensor(nextObs, dtype=torch.float32).view(-1, NUM_OBS).to(device)
+        nextObsFrame = torch.tensor(nextObs, dtype=torch.float32).view(-1, 4)
         complexReward = computeReward(nextObsFrame, done)
         reward = reward if not done else -reward
         reward = complexReward + reward
@@ -132,40 +114,37 @@ while not finishedTraining:
         if done:
             break
 
-        #   experience replay
+        #   "EXPERIENCE REPLAY": have PTSD Flashbacks
+        #   #   its good to remember some of the good times in life
+        #   #   (also a lot of bad ones)
         if len(memoryBank) >= BATCH_SIZE:
             memoryBatch = random.sample(memoryBank, BATCH_SIZE)
-            memoryBatch = MemoryFrame(*zip(*memoryBatch))
+            for memory in memoryBatch:
+                q_update = memory.reward
+                if not memory.done:
+                    futureOutput = net(memory.nextObsFrame).clone().detach()
+                    values, index = futureOutput.max(1)
+                    action = int(index)
+                    futureActionValue = float(values[0])
+                    #   'now reward' should include 'future reward'
+                    #   #   iteratively distil the effects of the future
+                    #   #   into our understanding of the effects of decisions now
+                    q_update = memory.reward + GAMMA * futureActionValue
+            
+                output = net(memory.obsFrame)
+                newQValues = output.clone().detach()
+                # print("newQValues:")
+                # print(newQValues)
+                newQValues[0][memory.action] = q_update
+                # print(newQValues)
+                loss = criterion(output, newQValues)
+                # print(loss)
+                net.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            memObsFrames =      torch.stack(    memoryBatch.obsFrame)
-            memActions =        torch.tensor(   memoryBatch.action, dtype=torch.long)
-            memRewards =        torch.tensor(   memoryBatch.reward, dtype=torch.float32)
-            memNextObsFrames =  torch.stack(    memoryBatch.nextObsFrame)
-            # memDones = torch.tensor(memoryBatch.done)
-
-            futureOutputs = net(memNextObsFrames).clone().detach()
-            futureActionValues, futureActions = futureOutputs.max(2)
-            futureActionValues = futureActionValues.view(BATCH_SIZE)
-            # qUpdate = memRewards + (GAMMA * futureActionValues.view(BATCH_SIZE)) * (memDones.logical_not())
-            qUpdate = memRewards + GAMMA * futureActionValues.view(BATCH_SIZE)
-
-            output = net(memObsFrames)
-            newQValues = output.clone().detach().view(BATCH_SIZE, 2)
-            memActions = memActions.view(BATCH_SIZE, 1)
-            qUpdate = qUpdate.view(BATCH_SIZE, 1)
-            newQValues = newQValues.scatter_(1, memActions, qUpdate).view(BATCH_SIZE, 1, NUM_ACTIONS)
-
-            loss = F.mse_loss(output, newQValues, reduction="mean")
-            net.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        epsilon *= EPSILON_DECAY
-        epsilon = max(EPSILON_MIN, epsilon)
-
-        # stepCompute_time_end = datetime.datetime.now()
-        # stepComputeTime = stepCompute_time_end - stepCompute_time_start
-        # print(stepComputeTime)
+            epsilon *= EPSILON_DECAY
+            epsilon = max(EPSILON_MIN, epsilon)
             
     #   post episode logic
     episode += 1
@@ -183,12 +162,6 @@ while not finishedTraining:
         str(epsilon)[:4],
         len(memoryBank),
         ))
-
-############################################################
-####    SAVE MODEL
-############################################################
-
-
 
 ############################################################
 ####    EVAL
